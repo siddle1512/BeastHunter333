6c7bc319c86dc3034e9d3299f806b1ea3bb91920
ngocdung1004 <nguyenngocdung100420032k3@gmail.com>
2024-09-27 10:41:12 +0700

    zoomin + zoomout cho camera, nhanvat di chuyen theo huong con tro chuot

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,13 +8,36 @@ public class CameraController : MonoBehaviour
     public class CamSetting
     {
         public float moveSpeed = 5;
+        public float mouseX_Sensivity = 5;
+        public float mouseY_Sensivity = 5;
+        public float minClamp = -30;
+        public float maxClamp = 90;
+        public float rotateSpeed = 5;
+        public float zoomFileOfView = 30;
+        public float originalzoomFileOfView = 60;
+        public float zoomSpeed = 5;
+
     }
     [SerializeField]
     CamSetting camsettings;
 
+    [System.Serializable]
+    public class camInputSettings
+    {
+        public string mouseXAxis = "Mouse X";
+        public string mouseYAxis = "Mouse Y";
+        public string aimInput = "Fire2";
+    }
+    [SerializeField]
+
+    camInputSettings cis;
+
     Camera maincam;
     Transform center;
     Transform target;
+
+    float camXRotate = 0;
+    float camYRotate = 0;
     void Start()
     {
         maincam = Camera.main;
@@ -24,7 +47,11 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        
+        if (!target)
+            return;
+
+        rotateCam();
+        zoomCam();
     }
 
     private void LateUpdate()
@@ -48,4 +75,30 @@ public class CameraController : MonoBehaviour
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
     }
+
+    void rotateCam()
+    {
+        camXRotate += Input.GetAxis(cis.mouseYAxis) * camsettings.mouseY_Sensivity;
+        camYRotate += Input.GetAxis(cis.mouseXAxis) * camsettings.mouseX_Sensivity;
+
+        camXRotate = Mathf.Clamp(camXRotate, camsettings.minClamp, camsettings.maxClamp);
+        camYRotate = Mathf.Repeat(camYRotate, 360);
+
+        Vector3 rotatingAngle = new Vector3(camXRotate, camYRotate, 0);
+        Quaternion rotate = Quaternion.Slerp(center.transform.localRotation, Quaternion.Euler(rotatingAngle), camsettings.rotateSpeed * Time.deltaTime);
+        center.transform.localRotation = rotate;
+    }
+
+    void zoomCam()
+    {
+        if (Input.GetButton(cis.aimInput))
+        {
+            maincam.fieldOfView = Mathf.Lerp(maincam.fieldOfView, camsettings.zoomFileOfView, camsettings.zoomSpeed * Time.deltaTime);
+        }
+        else
+        {
+            maincam.fieldOfView = Mathf.Lerp(maincam.fieldOfView, camsettings.originalzoomFileOfView, camsettings.zoomSpeed * Time.deltaTime);
+        }
+    }
+
 }
--- Assets/Scripts/Character/InputSystem.cs
+++ Assets/Scripts/Character/InputSystem.cs
@@ -17,14 +17,36 @@ public class InputSystem : MonoBehaviour
 
     InputSetting input;
 
+    [Header("camera & character syncing")]
+    public float lookDistace = 5;
+    public float lookSpeed = 5;
+
+    Transform camCenter;
+
     void Start()
     {
         move = GetComponent<Movement>();
+        camCenter = Camera.main.transform.parent;
     }
 
     void Update()
     {
+        if (Input.GetAxis(input.horizontal) !=0 || Input.GetAxis(input.vertical) != 0)
+                rotateToCamView();
         move.CharacterMove(Input.GetAxis(input.horizontal), Input.GetAxis(input.vertical));
         move.CharacterSprint(Input.GetButton(input.sprint));
+    }
+
+    void rotateToCamView()
+    {
+        Vector3 camCenterPos = camCenter.position;
+        Vector3 lookPoint = camCenterPos + (camCenter.forward * lookDistace);
+        Vector3 dir = lookPoint - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        lookRotation.x = 0;
+        lookRotation.z = 0;
+        Quaternion final = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * lookSpeed);
+        transform.rotation = final;
+
     }
 }

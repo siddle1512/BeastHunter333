dedc5798fff9e1aad5b3aa098555f3b1a0d94459
siddle1512 <binhbobinhbo22@gmail.com>
2024-10-30 23:45:39 +0700

    [Add] Add-Player-die

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    public int HP = 100;
+
+    public Animator animator;
+    Transform agent;
+    Transform player;
+    public void TakeDamge(int damageAmount)
+    {
+        HP -= damageAmount;
+        if (HP <= 0)
+        {
+            animator.SetTrigger("die");
+        }
+    }
+}

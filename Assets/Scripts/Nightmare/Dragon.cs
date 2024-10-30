7dddce54f816090d636bc5bfee572f9e2578b125
siddle1512 <binhbobinhbo22@gmail.com>
2024-10-30 21:31:40 +0700

    [Add] Add-Damage-Dragon

---
+++ Assets/Scripts/Nightmare/Dragon.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    public int HP = 100;
+
+    public Animator animator;
+    public void TakeDamge(int damageAmount)
+    {
+        HP -= damageAmount;
+        if (HP <= 0)
+        {
+            animator.SetTrigger("die");
+            GetComponent<Collider>().enabled = false;
+        }
+        else
+        {
+            animator.SetTrigger("damage");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
